75fbdd6ac264fb546a9f6bbb61babf98f34ef986
Geoff Maddock <geoff.maddock@gmail.com>
2024-11-29 14:18:16 -0500

    Moved all config to env

--- src/api/eventService.js
+++ src/api/eventService.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-//const API_BASE_URL = 'https://arcane.city/api';
-const API_BASE_URL = '/api';
-
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const username = process.env.REACT_APP_API_USERNAME;
 const password = process.env.REACT_APP_API_PASSWORD;
 
